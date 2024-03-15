379b0cd59c207edd86e70170ed3a6739fced4891
Corvus006 <wagneraron@outlook.de>
2024-03-15 22:55:59 +0100

    first commit

---
+++ src/cam_pub.cpp
@@ -0,0 +1,45 @@
+#include <chrono>
+#include <cstdio>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.hpp>
+#include <opencv2/opencv.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/compressed_image.hpp>
+
+using namespace std::chrono_literals;
+
+
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::NodeOptions options;
+  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("n10_cam_pub_node", options);
+  image_transport::ImageTransport it(node);
+  image_transport::Publisher pub = it.advertise("/n10/cam_pub", 1);
+
+  cv::VideoCapture cap(0);
+  if (!cap.isOpened())
+    return 1;
+  cv::Mat frame;
+  std_msgs::msg::Header hdr;
+  sensor_msgs::msg::Image::SharedPtr msg;
+
+  rclcpp::WallRate loop_rate(5ms);
+  while (rclcpp::ok())
+  {
+    cap >> frame;
+    // Check if grabbed frame is actually full with some content
+    if (!frame.empty())
+    {
+      msg = cv_bridge::CvImage(hdr, "bgr8", frame).toImageMsg();
+      pub.publish(msg);
+      cv::waitKey(1);
+    }
+
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
