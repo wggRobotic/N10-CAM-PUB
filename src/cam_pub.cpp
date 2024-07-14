2d2b0d512a666a79a444b093266fcac99def80c8
Jonas Nicklas <wagneraron@outlook.de>
2024-07-14 13:31:12 +0200

    added param

--- src/cam_pub.cpp
+++ src/cam_pub.cpp
@@ -8,19 +8,31 @@
 
 using namespace std::chrono_literals;
 
-
-
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions options;
-  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("n10_cam_pub", options);
-  image_transport::ImageTransport it(node);
-  image_transport::Publisher pub = it.advertise("/n10/rear/color", 1);
+  auto node = rclcpp::Node::make_shared("n10_cam_pub", options);
+  
+  // Declare and get parameters
+  node->declare_parameter<std::string>("topic_name", "/n10/rear/color");
+  node->declare_parameter<std::string>("camera_name", "video0");
+  std::string topic_name = node->get_parameter("topic_name").as_string();
+  std::string camera_name = node->get_parameter("camera_name").as_string();
 
-  cv::VideoCapture cap(0);
+  // Initialize publisher
+  image_transport::ImageTransport it(node);
+  image_transport::Publisher pub = it.advertise(topic_name, 1);
+
+  // Open the video capture
+  std::string camera_path = "/dev/" + camera_name;
+  cv::VideoCapture cap(camera_path);
   if (!cap.isOpened())
+  {
+    RCLCPP_ERROR(node->get_logger(), "Could not open camera: %s", camera_path.c_str());
     return 1;
+  }
+
   cv::Mat frame;
   std_msgs::msg::Header hdr;
   sensor_msgs::msg::Image::SharedPtr msg;
